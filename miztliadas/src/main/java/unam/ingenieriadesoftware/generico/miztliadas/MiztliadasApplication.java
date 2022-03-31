65c3248d44bed3d63bf5b7c115629e9810b74366
Ulises Lopez <ulises.lopez@ciencias.unam.mx>
2022-03-30 20:02:43 -0600

    Spring initilazer

---
+++ miztliadas/src/main/java/unam/ingenieriadesoftware/generico/miztliadas/MiztliadasApplication.java
@@ -0,0 +1,13 @@
+package unam.ingenieriadesoftware.generico.miztliadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiztliadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiztliadasApplication.class, args);
+	}
+
+}
---
+++ miztliadas/src/test/java/unam/ingenieriadesoftware/generico/miztliadas/MiztliadasApplicationTests.java
@@ -0,0 +1,13 @@
+package unam.ingenieriadesoftware.generico.miztliadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiztliadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
