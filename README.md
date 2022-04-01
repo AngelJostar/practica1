ddbb2aefeed2fdb002ab4393b204d44f1f019fd9
Izumi S <sainz@ciencias.unam.mx>
2022-04-01 00:53:24 -0600

    Completed practica1

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Práctica 1
 
-Delgado Díaz Itzel Azucena 314262358
+Delgado Díaz Itzel Azucena          314262358
 
-Lopez Arias Victor Ulises 310173335
+Lopez Arias Victor Ulises           310173335
 
-Rojas Espinoza Luis Angel 314170147
+Rojas Espinoza Luis Angel           314170147
 
-Rosas Bautista Miguel Filiberto 315284162
+Rosas Bautista Miguel Filiberto     315284162
 
-Sainz Takata Izumi María 314245195
+Sainz Takata Izumi María            314245195
--- miztliadas/src/main/java/unam/ingenieriadesoftware/generico/miztliadas/PracticaController.java
+++ miztliadas/src/main/java/unam/ingenieriadesoftware/generico/miztliadas/PracticaController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class PracticaController {
 
 	@GetMapping("/integrante")
 	public String nombreIntegrante(@RequestParam(name="nombre", required=false, defaultValue="Integrante") String nombre, Model model) {
-		model.addAttribute("nombre", nombre);
+		List<String> integrantes = Arrays.asList(nombre.split(","));
+		model.addAttribute("integrantes", integrantes);
 		return "integrante.html";
 	}
 	
